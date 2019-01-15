115c59af938ba1600244bfba3c3933638a9e0c67
kzk_maeda <casino.music0711@gmail.com>
2019-01-16 00:56:11 +0900

    refuctoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,63 @@
 import React, { Component } from 'react';
 
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+import Map from './Map';
+import { geocode } from '../domain/Geocoder';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      location: {
+        lat: 35.6585805,
+        lng: 139.7454329,
+      },
     };
   }
 
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      location: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
   handlePlaceSubmit(place) {
-    console.log(place);
+    geocode(place)
+      .then(({ status, address, location }) => {
+        switch (status) {
+          case 'OK': {
+            this.setState({ address, location });
+            break;
+          }
+          case 'ZERO_RESULTS':
+            this.setErrorMessage('Non Results Returned.');
+            break;
+          default:
+            this.setErrorMessage('Error Occered.');
+            break;
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('通信に失敗しました');
+      });
   }
 
   render() {
     return (
       <div>
         <h1>緯度経度検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          location={this.state.location}
+        />
+        <Map location={this.state.location} />
       </div>
     );
   }
